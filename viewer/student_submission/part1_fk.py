d0d73a5878b2ed54e23ed04420cdc6bb9250523f
Aaron-AA0721 <aaronaa8231@gmail.com>
2026-05-15 19:54:34 +0100

    part 1 task complete

--- viewer/student_submission/part1_fk.py
+++ viewer/student_submission/part1_fk.py
@@ -9,6 +9,31 @@ def forward_kinematics(
     root_offset: np.ndarray,
     topological_order: tuple[int, ...] | None = None,
 ) -> tuple[np.ndarray, np.ndarray]:
+    world_rotations = np.zeros((len(joints), 3, 3))
+    world_positions = np.zeros((len(joints), 3))
+    joint_completed = [False] * len(joints)
+    def get_world_transform(joint):
+        
+        if(joint == -1):
+            return np.eye(3), np.zeros((3,))
+        if(joint_completed[joint]):
+            return world_rotations[joint], world_positions[joint]
+        parent = joints[joint].parent
+        local_rotation = local_rotations[joint]
+        local_position = joints[joint].translation
+        
+        if(parent != -1):
+            parent_rotation, parent_position = get_world_transform(parent)
+            world_rotation = parent_rotation @ local_rotation
+            world_position = parent_position + parent_rotation @ local_position
+            joint_completed[joint] = True
+            return world_rotation, world_position
+        else:
+            world_position = local_position + root_offset
+            joint_completed[joint] = True
+            return local_rotation, world_position
+        
+    
     """Student part-1 implementation.
 
     Expected inputs:
@@ -21,10 +46,23 @@ def forward_kinematics(
     - world_rotations: shape (J, 3, 3)
     - world_positions: shape (J, 3)
     """
-    joint_count = len(joints)
-    world_rotations = np.tile(
-        np.eye(3, dtype=np.float32)[None, :, :],
-        (joint_count, 1, 1),
+    
+    for joint in range(len(joints)):
+        world_rotations[joint], world_positions[joint] = get_world_transform(joint)
+
+        # if(joints[joint].parent != -1):
+        #     parent = joints[joint].parent
+        #     world_rotations[joint] = world_rotations[parent] @ local_rotation
+        #     world_positions[joint]  = world_positions[parent] + world_rotations[parent] @ local_position
+            
+        # else:
+        #     world_positions[joint] = local_position + root_offset
+        #     world_rotations[joint] = local_rotation
+
+    return world_rotations, world_positions
+
+    raise NotImplementedError(
+        "Implement forward_kinematics() in student_submission/part1_fk.py"
     )
     world_positions = np.zeros((joint_count, 3), dtype=np.float32)
     return world_rotations, world_positions
